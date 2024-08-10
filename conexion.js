1c4c35830e20ee5ca2a27c004dca6f79b1c0e8d8
GudinoJoaquin <JoaGudi@hotmail.com>
2024-08-10 19:43:54 -0300

    ANDA

--- conexion.js
+++ conexion.js
@@ -1,19 +1,19 @@
 import mysql from "mysql2";
 
-// const conexion = mysql.createConnection({
-//   host: "monorail.proxy.rlwy.net",
-//   user: "root",
-//   password: "jBQJNtzcatsolqkjzhAuaJPSuhiKzpvC",
-//   database: "railway",
-//   port: 3306,
-// });
-
 const conexion = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "anunciost5mdp",
+  host: "www.eest5mdp.edu.ar",
+  user: "DiegoAdministradorT5",
+  password: "EESTN5anunciosADMIN",
+  database: "anunciosT5mdp",
+  port: 3306,
 });
+
+// const conexion = mysql.createConnection({
+//   host: "localhost",
+//   user: "root",
+//   password: "",
+//   database: "anunciost5mdp",
+// });
 
 conexion.connect((err) => {
   if (err) {
